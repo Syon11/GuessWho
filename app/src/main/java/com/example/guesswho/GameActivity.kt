cdbdc1f54064d2d3ea47e88dcc3d5860034facb9
Syon11 <philp.lafond@gmail.com>
2023-10-05 09:01:54 -0400

    Refactor: Extract createTextView, createImageView, createCard functions.

--- app/src/main/java/com/example/guesswho/GameActivity.kt
+++ app/src/main/java/com/example/guesswho/GameActivity.kt
@@ -10,8 +10,10 @@ import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.ScrollView
 import android.widget.Spinner
 import android.widget.TextView
@@ -34,47 +36,9 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game)
-        var done = false
-        val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
-        val primaryLinearLayout: LinearLayout = findViewById(R.id.primaryLinearLayout)
-
-        for (i in 0..(numberOfCharacters/3)*2 step 2 ) {
-            var layout = LinearLayout(this)
-            layout.layoutParams = LinearLayout.LayoutParams(displayMetrics.widthPixels,150)
-            primaryLinearLayout.addView(layout)
-            for (j in 0..2){
-                if (j+((i/2)*3) == numberOfCharacters){
-                    done = true;
-                    break;
-                }
-                var imageView = ImageView(this)
-                var character: Character = Singleton.getCharacter(j+((i/2)*3))
-                val imageParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-                imageParams.setMargins(30, 10, 0, 5)
-                imageParams.gravity = Gravity.CENTER
-                imageView.layoutParams = imageParams
-                imageView.setImageResource(character.getDrawableLocation())
-                layout.addView(imageView);
-            }
-            if (done){
-                break
-            }
-            var layout2 = LinearLayout(this)
-            layout2.layoutParams = LinearLayout.LayoutParams(displayMetrics.widthPixels, 500)
-            primaryLinearLayout.addView(layout2)
-            for (j in 0..2){
-                var textView = TextView(this)
-                var character = Singleton.getCharacter(j+((i/2)*3))
-                val textParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-                textParams.setMargins(50,0,0,0)
-                textParams.gravity = Gravity.CENTER
-                textView.layoutParams = textParams
-                textView.text = character.getName()
-                layout.addView(textView)
-            }
-        }
+        drawCharacters()
         setSpinner();
+
     }
 
     private fun setSpinner(){
@@ -82,5 +46,65 @@ class GameActivity : AppCompatActivity() {
         adapter = ArrayAdapter.createFromResource(this, R.array.questions, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
         spinner.adapter = adapter
+    }
+
+    private fun drawCharacters() {
+        var done = false
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val primaryLinearLayout: LinearLayout = findViewById(R.id.primaryLinearLayout)
+
+        for (i in 0..(numberOfCharacters/3)+1 ) {
+            var relLayout = RelativeLayout(this)
+            var layout = LinearLayout(this)
+            var layoutOverlay = LinearLayout(this)
+            layout.layoutParams = LinearLayout.LayoutParams(displayMetrics.widthPixels,180)
+            primaryLinearLayout.addView(layout)
+            for (j in 0..2) {
+                if (j+(i*3) >= numberOfCharacters){
+                    done = true;
+                    break;
+                }
+                createCard(layout, j+(i*3))
+            }
+            if (done){
+                break;
+            }
+        }
+    }
+    private fun toggleButton(button: Button){
+        button.isEnabled = !button.isEnabled
+    }
+    private fun createTextView(width: Int, height: Int, character: Character): TextView{
+        var textView = TextView(this)
+        val textParams = LinearLayout.LayoutParams( width, height)
+        textParams.setMargins(30,0,5,0)
+        textParams.gravity = Gravity.CENTER
+        textView.layoutParams = textParams
+        textView.text = character.getName()
+        return textView
+    }
+
+    private fun createImageView(width: Int, height: Int, character: Character): ImageView{
+        var imageView = ImageView(this)
+        val imageParams = LinearLayout.LayoutParams(width, height)
+        imageParams.setMargins(0, 0, 0, 0)
+        imageParams.gravity = Gravity.CENTER
+        imageView.layoutParams = imageParams
+        imageView.setImageResource(character.getDrawableLocation())
+        return imageView
+    }
+
+    private fun createCard(parent: LinearLayout, index: Int): LinearLayout{
+        var verticalLayout = LinearLayout(this)
+        verticalLayout.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        verticalLayout.orientation = LinearLayout.VERTICAL
+        verticalLayout.gravity = Gravity.CENTER
+        parent.addView(verticalLayout)
+        var character: Character = Singleton.getCharacter(index)
+
+        verticalLayout.addView(createImageView(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, character));
+        verticalLayout.addView(createTextView(300, ViewGroup.LayoutParams.WRAP_CONTENT, character))
+
     }
 }
